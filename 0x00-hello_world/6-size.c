4f16aadc3fa757f5826c19fac1471da26e6aaf90
MinnaMohammed <barakatminna1@gmail.com>
2023-03-16 19:51:10 -0700

    script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ printf("Size of a char: %u byte(s)\n", sizeof(character));
 printf("Size of an int: %u byte(s)\n", sizeof(integer));
 printf("Size of a long int: %u byte(s)\n", sizeof(long_var));
 printf("Size of a long long int: %u byte(s)\n", sizeof(long_long_var));
-printf("Size of a float: %u byte(s)\n", sizeof(float_var));
+printf("Size of a float:  byte(s)\n", sizeof(float_var));
 
 return (0);
 }
