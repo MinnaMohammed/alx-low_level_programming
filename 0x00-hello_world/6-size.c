d2d9e7d57583771b90d1fdb3d6a221f0ce39912c
MinnaMohammed <barakatminna1@gmail.com>
2023-03-16 19:42:54 -0700

    c program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ char character;
 long long_var;
 long long long_long_var;
 float float_var;
-printf("Size of a char: %zu byte(s)\n",sizeof(character));
-printf("Size of an int: %zu byte(s)\n",sizeof(integer));
-printf("Size of a long int: %zu byte(s)\n",sizeof(long_var));
-printf("Size of a long long int: %zu byte(s)\n",sizeof(long_long_var));
-printf("Size of a float: %zu byte(s)\n",sizeof(float_var));
+printf("Size of a char: %zu byte(s)\n", sizeof(character));
+printf("Size of an int: %zu byte(s)\n", sizeof(integer));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long_var));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long_long_var));
+printf("Size of a float: %zu byte(s)\n", sizeof(float_var));
 
 return (0);
 }
