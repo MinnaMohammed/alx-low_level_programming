b5ec96aa5ecbad039a606709f83baca5e33057d1
MinnaMohammed <barakatminna1@gmail.com>
2023-03-22 20:04:14 -0700

    c program

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,9 +7,9 @@
 */
 void print_alphabet(void)
 {
+char letter;
 
- for(char letter = 'a'; letter<= 'z'; letter++)
-	 _putchar(letter);
- _putchar("\n");
- return (0);
+for(letter = 'a'; letter<= 'z'; letter++)
+     _putchar(letter);
+ _putchar('\n');
 }
