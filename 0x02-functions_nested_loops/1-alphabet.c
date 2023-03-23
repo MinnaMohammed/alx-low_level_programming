2ab99d47a30f760eb7464af980e88b5c14fd9e52
MinnaMohammed <barakatminna1@gmail.com>
2023-03-22 19:57:00 -0700

    c program

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+
+ for(char letter = 'a'; letter<= 'z'; letter++)
+	 _putchar(letter);
+ _putchar("\n");
+ return (0);
+}
