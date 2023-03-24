c35a08df2589ea2b9b9bf6e305c46e80b5de7c49
MinnaMohammed <barakatminna1@gmail.com>
2023-03-24 14:33:44 -0700

    c program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,5 +8,38 @@
 int main(void)
 {
 	putchar('a');
+	putchar('n');
+	putchar('d');
+	putchar(' ');
+	putchar('t');
+	putchar('h');
+	putchar('a');
+	putchar('t');
+	putchar(' ');
+	putchar('p');
+	putchar('i');
+	putchar('e');
+	putchar('c');
+	putchar('e');
+	putchar(' ');
+	putchar('o');
+	putchar('f');
+	putchar(' ');
+	putchar('a');
+	putchar('r');
+	putchar('t');
+	putchar(' ');
+	putchar('i');
+	putchar('s');
+	putchar(' ');
+	putchar('u');
+	putchar('s');
+	putchar('e');
+	putchar('f');
+	putchar('u');
+	putchar('l');
+	putchar('"');
+	putchar(' ');
+	putchar('-');
 return (1);
 }
