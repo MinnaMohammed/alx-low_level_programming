292cef20bb198af93aa040af6b5e4adc413b8e48
MinnaMohammed <barakatminna1@gmail.com>
2023-03-23 19:48:23 -0700

    c program

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,8 @@ for (number = 0; number <= 14; number++)
 	if (number >= 10)
 	{
 	_putchar('1');
-	_putchar((number % 10) + '0');
 	}
-	else
-	{
 	_putchar((number % 10) + '0');
-	}
-
 }
 _putchar('\n');
 }
