8efca56cd109f6f9e9892586faa2a4167af614a5
MinnaMohammed <barakatminna1@gmail.com>
2023-03-24 06:54:11 -0700

    c program

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,10 @@
 */
 void print_number(int n)
 {
+if (n == 0)
+{
+_putchar(0 + '0');
+}
 
 if (n < 0)
 {
@@ -13,10 +17,11 @@ _putchar('-');
 n = -n;
 }
 
-if (n / 10 == 0)
+if (n / 10)
 {
 print_number(n / 10);
 }
 _putchar(n % 10 + '0');
+
 
 }
