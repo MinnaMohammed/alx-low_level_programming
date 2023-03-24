9f50feb8a7f29d437305dd271f440aa7b9388a60
MinnaMohammed <barakatminna1@gmail.com>
2023-03-24 07:25:02 -0700

    c program

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,7 +6,12 @@
 void print_diagonal(int n)
 {
 int i, j;
-
+if (n <= 0)
+{
+	_putchar('\n');
+}
+else
+{
 for (i = 0; i <= n; i++)
 {
 	for (j = 0; j < i; j++)
@@ -17,5 +22,5 @@ for (i = 0; i <= n; i++)
 	_putchar('\n');
 
 }
-_putchar('\n');
+}
 }
