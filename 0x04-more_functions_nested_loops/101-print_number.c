5d078ccd90b23b13fa89f9cb97745e973401478f
MinnaMohammed <barakatminna1@gmail.com>
2023-03-24 06:35:38 -0700

    c program

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number - a function that prints integer numbers
+ *@n: integer number
+*/
+void print_number(int n)
+{
+	_putchar(n + '0');
+
+}
