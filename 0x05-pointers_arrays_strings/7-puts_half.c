d0c097e77a49625dc49da23158da558923dbec2e
MinnaMohammed <barakatminna1@gmail.com>
2023-03-28 19:47:10 -0700

    c program

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half -  print the second half of the string
+ *@str: input string
+*/
+void puts_half(char *str)
+{
+	int i;
+
+	int length = strlen(str);
+
+	if (length % 2 != 0)
+	{
+		length = (length - 1) / 2;
+	}
+	else
+	{
+		length = length / 2;
+	}
+
+
+	for (i = length; i < length - 1; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
