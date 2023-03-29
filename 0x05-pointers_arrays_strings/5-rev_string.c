9b4e64c09da4d81b4b2232f5aaafdacd9f812785
MinnaMohammed <barakatminna1@gmail.com>
2023-03-28 18:56:35 -0700

    c program

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * rev_string - reverses a string.
+ *@s: input string
+*/
+void rev_string(char *s)
+{
+	int i;
+	char *newstring;
+
+	strcopy(s, newstring);
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		s[i - (strlen(s) - 1)] = newstring[i];
+	}
+}
