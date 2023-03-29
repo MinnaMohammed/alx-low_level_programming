9b55d37cb27d80c1d59e84ee4389328ad4111922
MinnaMohammed <barakatminna1@gmail.com>
2023-03-28 20:01:40 -0700

    c program

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,7 +11,16 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		if(a[i] != 1)
+		{
+		printf(", %d", a[i]);
+		}
+		else
+		{
+		printf("%d", a[i]);
+		}
+
+
 	}
 	printf("\n");
 }
