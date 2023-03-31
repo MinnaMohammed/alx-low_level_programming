a1aca396a36eb28ccbfa591a6bf9b7deb29c0146
MinnaMohammed <barakatminna1@gmail.com>
2023-03-30 20:00:46 -0700

    c program

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,12 +5,12 @@
  * _strncpy -  copies a string.
  *@dest: first string (destination).
  *@src: second string (source).
- *@n: bytes copied from the src to the dest string. 
+ *@n: bytes copied from the src to the dest string.
  * Return: result in string (dest).
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int length_of_dest = strlen(dest);
+
 
 	for (i = 0; i < n; i++)
 	{
