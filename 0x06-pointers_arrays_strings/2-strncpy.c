718f2f725d979529b97bdb3f98418721ad544aee
MinnaMohammed <barakatminna1@gmail.com>
2023-03-30 20:09:46 -0700

    c program

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,6 +9,7 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
 
 	for (i = 0; i < n; i++)
@@ -16,6 +17,10 @@ char *_strncpy(char *dest, char *src, int n)
 		if (src[i] != '\0')
 		{
 		dest[i] = src[i];
+		}
+		else
+		{
+		dest[i]= '\0';
 		}
 	}
 	dest[i] = '\0';
