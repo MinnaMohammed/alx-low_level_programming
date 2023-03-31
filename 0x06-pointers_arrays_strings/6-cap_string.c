3bc03187b0d261863f89e15a785e71fb85387d29
MinnaMohammed <barakatminna1@gmail.com>
2023-03-31 07:06:39 -0700

    c program

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,14 +14,14 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (i == 0 && (str[0] >= 'a' && str[0] <= 'z'))
+		if (i == 0 && (str[0] >= 97 && str[0] <= 122))
 		{
 		str[0] = str[0] - 32;
 		}
 
-		if (str[i] == ' ' || str[i] == '.')
+		if (str[i] == ' ' || str[i] == '.' || str[i] == '\t')
 		{
-		if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+		if (str[i + 1] >= 97 && str[i + 1] <= 122)
 		{
 			str[i + 1] = str[i + 1] - 32;
 		}
