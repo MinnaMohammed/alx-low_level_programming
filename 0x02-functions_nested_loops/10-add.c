22641b111265ec7dc431cd619d1e7d5047cb7380
MinnaMohammed <barakatminna1@gmail.com>
2023-04-01 19:55:31 -0700

    c program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * add - adds two integers and returns the result.
+ *@num1: first input number.
+ *@num2: second input number.
+ * Return: sum of 2 numbers.
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
