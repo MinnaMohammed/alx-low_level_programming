581d5eff8a117db5c3ef7764d3bd29f3a0977ff0
MinnaMohammed <barakatminna1@gmail.com>
2023-04-10 19:39:40 -0700

    c program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *@argc: number of argument entered by the command line.
+ *@argv: array of strings containing the argumnents.
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	argv[0] = argv[0];
+
+	if (argc != 0)
+	{
+		printf("%d\n", argc);
+	}
+
+	return (0);
+}
