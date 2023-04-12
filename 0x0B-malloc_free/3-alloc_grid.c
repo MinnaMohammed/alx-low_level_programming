258d66d61ed8d6ef7500ecc8a9a901a89d8f8de4
MinnaMohammed <barakatminna1@gmail.com>
2023-04-12 04:45:54 -0700

    c program

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	new_array = malloc((width * height) * sizeof(int));
+	new_array = (int *)malloc((width * height) * sizeof(int));
 
 	if (new_array == NULL)
 	{
