cce9cfe956d313de21796cac77e64f013c0765d4
MinnaMohammed <barakatminna1@gmail.com>
2023-04-12 04:41:22 -0700

    c program

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 
 	int i, j;
 
-	if (width < 0 || height < 0 || width == 0 || length == 0)
+	if (width < 0 || height < 0 || width == 0 || height == 0)
 	{
 		return (NULL);
 	}
