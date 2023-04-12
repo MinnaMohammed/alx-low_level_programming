3295775e0e1edcc84cb715b41613674c970ffb86
MinnaMohammed <barakatminna1@gmail.com>
2023-04-11 18:52:39 -0700

    c program

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,14 +11,14 @@
 */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i = 0;
+
+	char *array;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
-	unsigned int i;
-
-	char *array;
 
 	array = malloc(size * sizeof(char));
 	if (array == NULL)
