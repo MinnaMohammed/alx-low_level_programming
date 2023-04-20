f932c8fa4e8a3a0b2ed6d385b917e1054a02ba46
MinnaMohammed <barakatminna1@gmail.com>
2023-04-20 08:31:45 -0700

    c porgram

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - Struct dog
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
