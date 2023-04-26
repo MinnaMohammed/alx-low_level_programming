5202df6a1c785a5760ceefbec119bb4857c87341
MinnaMohammed <barakatminna1@gmail.com>
2023-04-26 07:05:27 -0700

    c program

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,13 +14,7 @@ typedef struct dog
 	char *owner;
 } my_dog;
 
-typedef struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-}my_dog;
-
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
