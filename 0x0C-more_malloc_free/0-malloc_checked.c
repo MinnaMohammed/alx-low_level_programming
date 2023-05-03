f9568817ea4d69596be3c6049a1bb848334b0f7d
MinnaMohammed <barakatminna1@gmail.com>
2023-05-03 13:08:12 -0700

    c program

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,21 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * malloc_checked - Returns a pointer to the allocated memory
- * if malloc fails, the malloc_checked function should cause
- * normal process termination with a status value of 98
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
  *
- *@b: input integer
- *Return: Always 0 (Success)
- *
-*/
+ * Return: a pointer to the allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
-	int *ptr = malloc(b * sizeof(int));
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
 }
+
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,9 @@
 #ifndef main_h
 #define main_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
