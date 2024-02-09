dcf12db60810b8b268837b9e57c3d97e3511ed5b
Minnatullah <84049808+MinnaMohammed@users.noreply.github.com>
2024-02-09 05:57:54 +0300

    Bit manipulation [task 4]

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * clear_bit - sets the value to 0 at given index
+ * @n: a number
+ * @index: the index of the bit
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int clear;
+
+	if (index > 64)
+		return (-1);
+	clear = 1 << index;
+  clear = ~clear;
+	*n = *n | clear;
+	return (1);
+}
