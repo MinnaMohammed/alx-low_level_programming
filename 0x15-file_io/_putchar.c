df398da1fbac7be73016bb3f5d0ebd12ef84fbde
MinnaMohammed <barakatminna1@gmail.com>
2024-02-12 16:59:08 -0800

    Helper function

---
+++ 0x15-file_io/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ *
+ *@c: a character
+ *
+ * Return: On success 0.
+ */
+void _putchar(char c)
+{
+	putchar(c);
+}
