576004b50ecae1f21f6fa8806ec7c35eff477ac9
MinnaMohammed <barakatminna1@gmail.com>
2024-02-12 18:26:49 -0800

    File I/O [task 1]

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fp = open(filename, O_CREAT | O_RDONLY | O_WRONLY | O_TRUNC, 0600);
+	fp = open(filename, O_CREAT | O_RDONLY | O_WRONLY | O_TRUNC);
 
 	if (fp == -1)
 		return (-1);
