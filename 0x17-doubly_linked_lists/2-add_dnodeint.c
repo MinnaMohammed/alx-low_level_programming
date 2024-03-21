97ffec84cbf5c6b08d0cb9191dd2bb6fff6e3b94
MinnaMohammed <barakatminna1@gmail.com>
2024-03-21 14:42:50 -0700

    [2. Add node]

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,19 +13,21 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 
-	if (!(*head))
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 	new->n = n;
-	(*head)->prev = new;
-	new->next = (*head);
 	new->prev = NULL;
+	new->next = (*head);
+	
+	if (*head != NULL)
+	{
+		(*head)->prev = new;
+	}
+
 	(*head) = new;
 
-	return (*head);
+	return (new);
 }
