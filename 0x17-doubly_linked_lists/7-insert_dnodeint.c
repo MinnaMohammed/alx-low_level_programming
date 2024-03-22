598a1cf79744414a0c16668081e57e17f3f71abd
MinnaMohammed <barakatminna1@gmail.com>
2024-03-21 18:39:23 -0700

    [7. Insert at index]

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,8 +14,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     dlistint_t *new, *current;
     unsigned int idx_cnt = 0;
 
-    if (h == NULL)
-        return (NULL);
 
     current = *h;
 
