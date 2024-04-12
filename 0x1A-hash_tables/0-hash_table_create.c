42c5a544c708c8d5e14270ce6298e9c8830a040b
MinnaMohammed <barakatminna1@gmail.com>
2024-04-12 10:03:02 -0700

    [0. >>> ht = {}]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,15 +4,29 @@
  * hash_table_create - creates a hash table.
  *
  *@size: the size of the array
- * 
+ *
  * Return: a pointer to the newly created hash table or NULL if the fn fails
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht;
-	new_ht = malloc(sizeof(hash_table_t)*size);
+	unsigned long int i;
+
+	new_ht = malloc(sizeof(hash_table_t));
 
 	if (!new_ht)
 		return (NULL);
-	return(new_ht);
+
+	new_ht->size = size;
+	new_ht->array = malloc(sizeof(hash_table_t *) * size);
+
+	if (!(new_ht->array))
+	{
+		free(new_ht);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		new_ht->array[i] = NULL;
+	return (new_ht);
 }
