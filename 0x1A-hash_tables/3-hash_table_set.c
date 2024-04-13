9ad1c0bf2c87e406a5ec4273a62f8d99777fdbcc
MinnaMohammed <barakatminna1@gmail.com>
2024-04-12 18:56:12 -0700

    [3. >>> ht['betty'] = 'cool']

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *new_node, *current_node;
 
-	if (!key || strcmp(key, "") == 0)
+	if (!ht || !key || strcmp(key, "") == 0)
 		return (0);
 	idx = key_index((unsigned char *)key, ht->size);
 
